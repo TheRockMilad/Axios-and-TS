d9c6d68c5240e132f7c6ae20c2f05ca0b3e11e34
TheRockMilad <therock.milad2@outlook.com>
2024-09-19 19:11:12 +0330

    add controller

--- controller.ts
+++ controller.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Request, Response, NextFunction, response } from "express";
+import { Request, Response, NextFunction } from "express";
 
 interface playerData {
   name: string;
@@ -11,17 +11,17 @@ interface playerData {
   redCards: number;
 }
 
-const allInformation = async (
+export const allInformation = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
     const response = await axios.get(
-      "https://web-api.varzesh3.com/v1.0/football/teams/10/stats/goals?seasonId=900827",
-    //   {
-    //     headers: { apikey: "p1EEPUBuVVDXy0OWmNqI4v4J1dvBHH3v" },
-    //   }
+      "https://web-api.varzesh3.com/v1.0/football/teams/10/stats/goals?seasonId=900827"
+      //   {
+      //     headers: { apikey: "p1EEPUBuVVDXy0OWmNqI4v4J1dvBHH3v" },
+      //   }
     );
     const playerData: playerData[] = response.data.players.map(
       (player: playerData) => ({
@@ -41,4 +41,34 @@ const allInformation = async (
   }
 };
 
-export default allInformation;
+export const allInformation2 = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const response = await axios.get(
+      "https://web-api.varzesh3.com/v1.0/football/teams/10/stats/goals?seasonId=900827",
+      {
+        timeout: 1000,
+        headers: { apikey: "p1EEPUBuVVDXy0OWmNqI4v4J1dvBHH3v" },
+      }
+    );
+
+    const playerData: playerData[] = response.data.players.map(
+      (player: playerData) => ({
+        name: player.name,
+        teamName: player.teamName,
+        portrate: player.portrait,
+        goals: player.goals,
+        assists: player.assists,
+        yellowCard: player.yellowCards,
+        redCard: player.redCards,
+      })
+    );
+
+    res.json(playerData); // Send the complete playerData array
+  } catch (error) {
+    console.log(error);
+  }
+};
--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 const app = express();
-import allInformation from "./controller"
+import { allInformation, allInformation2 } from "./controller";
 
-app.use(express.urlencoded({extended : true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use ('/data', allInformation)
+app.use("/api/data", allInformation);
+app.use("/api/data2", allInformation2);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
